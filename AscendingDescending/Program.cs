4becf5068c415d250c61cbdd46631c8aecdc6a47
Scott <scottjanda@gmail.com>
2023-01-14 19:57:09 -0500

    first commit

---
+++ AscendingDescending/Program.cs
@@ -0,0 +1,35 @@
+﻿string userContinue = "y";
+
+do
+{
+    Console.Write("Please enter a number: ");
+    bool validNumber = int.TryParse(Console.ReadLine(), out int number);
+    Console.Clear();
+    if (validNumber == false)
+    {
+        Console.WriteLine("That was not a valid number!");
+        Console.WriteLine();
+        continue;
+    }
+
+    for (int i = number; i >= 0; i--)
+    {
+        Console.Write(i + " ");
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i <= number; i++)
+    {
+        Console.Write(i + " ");
+    }
+
+    Console.WriteLine();
+
+    Console.Write("Do you want to continue (y/n)? ");
+    userContinue = Console.ReadLine();
+    Console.Clear();
+
+} while (userContinue.ToLower() == "y");
+
+Console.WriteLine("Goodbye!");
